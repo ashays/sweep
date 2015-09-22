87520ebccbb7b725fd3b30490dc06e9166f48d3d
Ashay Sheth <ashay.sheth@gmail.com>
2015-09-21 23:36:03 -0400

    script to interact with user

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
-var deck = [];
+$(function(){
 
-for (i = 1; i <= 4; i++) {
-	for (j = 1; j <= 13; j++) {
-		var card = {rank: j, suit: i};
-		deck[13 * (i - 1) + j - 1] = card;		
-	}
-}
+$(".card").click(function(event) {
+    console.log($(event.target).data());
+});
 
+})
 
-function deckShuffle()    {             
-    for (var i = 0; i < 52; i++)    {
-        var randomInt = Math.floor(Math.random() * 52);
-        var temp = deck[i];
-        deck[i] = deck[randomInt];
-        deck[randomInt] = temp;
-        //deck.set(i, deck.get(randomInt));
-        //deck.set(randomInt, temp);
-    }
-}
