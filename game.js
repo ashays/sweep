75ebda811008a64a15e0486ec6e1cfbfa2ad9dbb
dhruvsagar <dhruv.sagar@gatech.edu>
2015-09-21 23:11:34 -0400

    changed name and restructuring

--- game.js
+++ game.js
@@ -1,4 +1,8 @@
 var deck = [];
+var PFCards = [];
+var PSCards = [];
+var stagePiles = [];
+var tempStage = [];
 
 for (i = 1; i <= 4; i++) {
 	for (j = 1; j <= 13; j++) {
@@ -17,4 +21,25 @@ function deckShuffle()    {
         //deck.set(i, deck.get(randomInt));
         //deck.set(randomInt, temp);
     }
+}
+
+function deal() {
+	for (i = 0, ; i < 4; i++) {
+		var pile = {locked : false, value: deck[i].rank, cards: deck[i]};
+		stagePiles[i] = pile; 
+	}
+	for (i = 4; i < 28; i++) {
+		PFCards[i - 4] = deck[i];
+	}
+	for (i = 28; i < 52; i++) {
+		PSCards[i - 28] = deck[i];
+	}
+}
+
+function makeTempStage() {
+	
+}
+
+function firstTurn(selectedCard) {
+
 }
