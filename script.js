8d41a9bb4e1cf51aa757b3e7f6946e72b1f48735
dhruvsagar <dhruv.sagar@gatech.edu>
2015-09-21 22:42:36 -0400

    Added shuffle

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ for (i = 1; i <= 4; i++) {
 	}
 }
 
+
+function deckShuffle()    {             
+    for (var i = 0; i < 52; i++)    {
+        var randomInt = Math.floor(Math.random() * 52);
+        var temp = deck[i];
+        deck[i] = deck[randomInt];
+        deck[randomInt] = temp;
+        //deck.set(i, deck.get(randomInt));
+        //deck.set(randomInt, temp);
+    }
+}
